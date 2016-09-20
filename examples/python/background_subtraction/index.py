9e589bd1cf1b84630c110da1087c68a96db9e8cd
marc.wang <s4161035@gmail.com>
2016-09-20 15:41:20 +0800

    Update code refined

--- examples/python/background_subtraction/index.py
+++ examples/python/background_subtraction/index.py
@@ -1,6 +1,25 @@
 import numpy as np
 import cv2
 
+
+class BackgroundSubtractor:
+
+    def __init__(self):
+        history = 500
+        varThreshold = 30
+        detectShadows = False
+        self.detector = cv2.createBackgroundSubtractorMOG2(history, varThreshold, detectShadows)
+
+    def process(self, frame):
+        fgmask = self.detector.apply(frame)
+
+        kernel = np.ones((5, 5), np.uint8)
+        erodeImg = cv2.erode(fgmask, kernel)
+        dilateImg = cv2.dilate(erodeImg, kernel)
+
+        return dilateImg
+
+
 class MediaProcessor:
 
     def __init__(self, detector):
@@ -12,10 +31,14 @@ class MediaProcessor:
         while(True):
             ret, frame = video.read()
 
-            fgmask = self.detector.apply(frame)
+            fgmask = self.detector.process(frame)
+            
+            im2, contours, hierarchy = cv2.findContours(
+                fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
             cv2.imshow('fgmask', fgmask)
-            cv2.imshow('image', frame)
+            cv2.imshow('frame', frame)
             cv2.waitKey(33)
 
         video.release()
@@ -24,7 +47,6 @@ class MediaProcessor:
 
 if __name__ == '__main__':
 
-    fgbg = cv2.createBackgroundSubtractorMOG2(False)
-    mediaRes = MediaProcessor(fgbg)
-
+    detector = BackgroundSubtractor()
+    mediaRes = MediaProcessor(detector)
     mediaRes.run()
